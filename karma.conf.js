f40f5d11eb39486d8d41ce319d62f6fe36f5cab6
Wei Wang <wei@compass.com>
2017-12-08 16:49:45 -0500

    CircleCI uses Firefox 47, which is 10 versions behind
    Temporarily stop using FirefoxHeadless for testing.
    Don't have time to configure CircleCI right now.
    
    Maybe in the future we'll add this back when CircleCI
    gets its act together.

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     concurrency: Infinity,
   });
